23bc66628ef327e94a697c443e1c329117c9d9f6
Lyn <Lyn>
2019-02-20 02:55:08 +0900

    Wpf Binding Error 예제 코드

---
+++ WPF_Binding_Error/Original/WpfBindingError/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingError
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public class Data
+    {
+        public string HelloText { get; set; } = "Hello World!!";
+    }
+
+    public partial class MainWindow : Window
+    {
+        public Data ViewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new Data();
+        }
+    }
+}
